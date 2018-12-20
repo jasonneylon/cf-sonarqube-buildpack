22b7ba52c9ee2203e30e214a1da726b9aa3090bc
Jason Neylon <jason.neylon@gmail.com>
2018-12-20 16:57:35 +0100

    Updated the jre to the most recent version

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export JRE_URL=http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jre-8u181-linux-x64.tar.gz
+export JRE_URL=https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jre-8u191-linux-x64.tar.gz
 export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
